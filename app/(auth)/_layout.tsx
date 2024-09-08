35327409abd4a6908ce0c979554af694a936220a
kimba <skyqwn@gmail.com>
2024-09-08 21:52:57 +0900

    authcontext

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,12 @@
+import { Redirect, Stack } from 'expo-router';
+import { useAuth } from '~/contexts/AuthProvider';
+
+export default function AuthLayout() {
+  const { isAuthenticated } = useAuth();
+
+  if (isAuthenticated) {
+    return <Redirect href={'/'} />;
+  }
+
+  return <Stack></Stack>;
+}
--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,5 +1,15 @@
+import { Stack } from 'expo-router';
 import React, { useState } from 'react';
-import { Alert, StyleSheet, View, AppState, TextInput, Button } from 'react-native';
+import {
+  Alert,
+  StyleSheet,
+  View,
+  AppState,
+  TextInput,
+  Button,
+  Pressable,
+  Text,
+} from 'react-native';
 import { supabase } from '~/utils/supabase';
 
 // Tells Supabase Auth to continuously refresh the session automatically if
@@ -46,45 +56,37 @@ export default function Login() {
   }
 
   return (
-    <View style={styles.container}>
-      <View style={[styles.verticallySpaced, styles.mt20]}>
-        <TextInput
-          onChangeText={(text) => setEmail(text)}
-          value={email}
-          placeholder="email@address.com"
-          autoCapitalize={'none'}
-        />
-      </View>
-      <View style={styles.verticallySpaced}>
-        <TextInput
-          onChangeText={(text) => setPassword(text)}
-          value={password}
-          secureTextEntry={true}
-          placeholder="Password"
-          autoCapitalize={'none'}
-        />
-      </View>
-      <View style={[styles.verticallySpaced, styles.mt20]}>
-        <Button title="Sign in" disabled={loading} onPress={() => signInWithEmail()} />
-      </View>
-      <View style={styles.verticallySpaced}>
-        <Button title="Sign up" disabled={loading} onPress={() => signUpWithEmail()} />
+    <View className="flex-1 gap-3 bg-white p-5 pt-10">
+      <Stack.Screen options={{ title: 'Sign in' }} />
+      <TextInput
+        onChangeText={(text) => setEmail(text)}
+        value={email}
+        placeholder="email@address.com"
+        autoCapitalize={'none'}
+        className="rounded-md border border-gray-200 p-3"
+      />
+      <TextInput
+        onChangeText={(text) => setPassword(text)}
+        value={password}
+        secureTextEntry={true}
+        placeholder="Password"
+        autoCapitalize={'none'}
+        className="rounded-md border border-gray-200 p-3"
+      />
+      <View className="flex-row gap-3">
+        <Pressable
+          disabled={loading}
+          onPress={() => signInWithEmail()}
+          className="flex-1 items-center rounded-md border-2 border-red-500 p-3 px-8">
+          <Text className="text-center text-lg font-bold text-red-500 ">Sign In</Text>
+        </Pressable>
+        <Pressable
+          disabled={loading}
+          onPress={() => signUpWithEmail()}
+          className="flex-1 items-center rounded-md bg-red-500 p-3 px-8">
+          <Text className="text-lg font-bold text-white">Sign Up</Text>
+        </Pressable>
       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 40,
-    padding: 12,
-  },
-  verticallySpaced: {
-    paddingTop: 4,
-    paddingBottom: 4,
-    alignSelf: 'stretch',
-  },
-  mt20: {
-    marginTop: 20,
-  },
-});
